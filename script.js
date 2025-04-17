42ebf12fa9a5cd906115766e72379c0223142640
RagDo <rag.do200@gmail.com>
2025-04-17 18:20:53 +0300

    add a google sheet database

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-import { database } from "./database.js";
+import { getFamilyData } from "./database.js";
+// COSTUM templates
+// female
+OrgChart.templates.female = Object.assign({}, OrgChart.templates.ana);
+OrgChart.templates.female.node =
+  '<rect x="0" y="0" height="{h}" width="{w}" fill="#EE7674" stroke-width="1" stroke="#aeaeae" rx="7" ry="7" color="#000000"></rect>';
+// wife
+OrgChart.templates.wife = Object.assign({}, OrgChart.templates.ula);
+OrgChart.templates.wife.node =
+  '<rect x="0" y="0" height="{h}" width="{w}" fill="#ffffff" stroke-width="1" stroke="#aeaeae"></rect><line x1="0" y1="0" x2="250" y2="0" stroke-width="2" stroke="#EE7674"></line>';
 
-let options = getOptions();
-let chart = new OrgChart(document.getElementById("tree"), {
-  enableSearch: true,
-  enableAI: false,
-  enableDragDrop: true,
-  mouseScrool: OrgChart.action.scroll,
-  scaleInitial: options.scaleInitial,
-  tags: {
-    assistant: {
-      template: "ula",
+getFamilyData().then((familyNodes) => {
+  console.log(familyNodes.find((x) => x.id === "G2-01")); // should exist
+  console.log(familyNodes.find((x) => x.pid === "G2-01")); // should exist
+
+  new OrgChart(document.getElementById("tree"), {
+    template: "ana",
+    enableDragDrop: true,
+    enableSearch: true,
+    enableTouch: true,
+    mouseScrool: OrgChart.action.scroll,
+
+    tags: {
+      assistant: {
+        template: "ula",
+      },
+      female: {
+        template: "female",
+      },
+      wife: {
+        template: "wife",
+      },
     },
-  },
-  nodeMenu: {
-    details: { text: "Details" },
-    edit: { text: "Edit" },
-    add: { text: "Add" },
-    remove: { text: "Remove" },
-  },
-  nodeBinding: {
-    field_0: "name",
-    field_1: "gender",
-    field_2: "age",
-    field_3: "alive",
-    img_0: "img",
-  },
+
+    nodeBinding: {
+      field_0: "name",
+      field_1: "field_1",
+      img_0: "img",
+    },
+
+    nodes: familyNodes,
+  });
 });
-
-// console.log(database);
-chart.load(database);
-
-function getOptions() {
-  const searchParams = new URLSearchParams(window.location.search);
-  let fit = searchParams.get("fit");
-  let scaleInitial = 1;
-  if (fit == "yes") {
-    scaleInitial = OrgChart.match.boundary;
-  }
-  return { scaleInitial };
-}
